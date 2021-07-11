036677918ff473f887338069dc3456bd364efea0
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-11 21:04:36 +0530

    Add: Single Player start with 0 Position

---
+++ src/SnackLadder.java
@@ -0,0 +1,7 @@
+public class SnackLadder {
+    public static void main(String[] args) {
+        int Position = 0;
+        System.out.println("Welcome to Snack Ladder Game");
+        System.out.println("Your Start position is " + Position);
+}
+    }
