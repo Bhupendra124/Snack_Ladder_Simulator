1eb099e6ecabed43e9d6e73ff7d4664a0bf65e98
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-11 22:45:48 +0530

    Added: Player get exact winning position

--- src/SnackLadder.java
+++ src/SnackLadder.java
@@ -1,25 +1,49 @@
 public class SnackLadder {
     public static void main(String[] args) {
+
+
+
+
+
         int Position = 0;
         System.out.println("Welcome to Snack Ladder Game");
         System.out.println("Your Start position is " + Position);
 
 
-        int dice=(int)( Math.random()* 10)%6+1;
-        System.out.println("Dice value is" +dice);
+        while (Position <= 99) {
 
-        int value=(int)(Math.random()*10) %3+1;
-        System.out.println("Case Number"+value);
-
-
-        switch (value){
-            case 1:
-                System.out.println("OOPs you are on same place Roll again");
-            case 2;
+            int dice = 1+(int) (Math.random() *10) %6) ;
+            System.out.println("Dice value is" + dice);
             Position=(Position+dice);
-                System.out.println("Your position after Ladder"+Position);
-            case 3:
-            Position=(Position-dice);
+
+            System.out.println("Position is" +Position);
+            int value = 1+(int) (Math.random() * 10) %3 ;
+            System.out.println("Case Number" + value);
+
+
+
+
+
+            switch (value) {
+                case 1:
+                    System.out.println("OOPs you are on same place Roll again");
+                case 2:
+                    ;
+                    Position = (Position + dice);
+                    System.out.println("Your position after Ladder" + Position);
+                case 3:
+                    Position = (Position - dice);
+
+                    if (Position < 0) {
+                        Position = 0;
+
+                    } else {
+
+                        
+                        System.out.println("Your position after Snake is" + Position);
+                        break;
+                    }
+            }
         }
     }
-    }
+}
