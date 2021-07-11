63f92f700d293dba4c813dcda83554a12704f929
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-07-11 22:17:22 +0530

    Added: check for Option They No Play ,Snack or Ladder

--- src/SnackLadder.java
+++ src/SnackLadder.java
@@ -3,5 +3,23 @@ public class SnackLadder {
         int Position = 0;
         System.out.println("Welcome to Snack Ladder Game");
         System.out.println("Your Start position is " + Position);
-}
+
+
+        int dice=(int)( Math.random()* 10)%6+1;
+        System.out.println("Dice value is" +dice);
+
+        int value=(int)(Math.random()*10) %3+1;
+        System.out.println("Case Number"+value);
+
+
+        switch (value){
+            case 1:
+                System.out.println("OOPs you are on same place Roll again");
+            case 2;
+            Position=(Position+dice);
+                System.out.println("Your position after Ladder"+Position);
+            case 3:
+            Position=(Position-dice);
+        }
+    }
     }
